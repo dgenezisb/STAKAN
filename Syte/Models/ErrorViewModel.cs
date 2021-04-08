1beffade1a6553e885df09b9b322a7346cb18ef5
Maksim <82108446+maks2179@users.noreply.github.com>
2021-04-08 23:26:08 +0300

    Создал проект

---
+++ Syte/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Syte.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
