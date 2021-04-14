b9979d371202778a0074c413cdb12b7abaa694dd
Jager227 <82110630+Jager227@users.noreply.github.com>
2021-04-14 18:40:25 +0300

    Категории и Книга

---
+++ Syte/Models/Interfaces/IAllBooks.cs
@@ -0,0 +1,15 @@
+﻿using Biblio.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblio.Data.Interfaces
+{
+    interface IAllBooks
+    {
+        IEnumerable<Book> Books { get; set; }
+        IEnumerable<Book> GetFavouriteBooks { get; set; }
+        Book GetObjectBook(int BookID);
+    }
+}
---
+++ Syte/Models/Mocks/Mock_Category.cs
@@ -0,0 +1,30 @@
+﻿using Biblio.Data.Interfaces;
+using Biblio.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblio.Data.Mocks
+{
+    public class Mock_Category : IBooksCategories
+    {
+        public IEnumerable<Category> ListofCategories {
+            get{
+                return new List<Category>                                       //ЗДЕСЬ ОПИСАТЬ КАТЕГОРИИ НА САЙТЕ
+                {
+                    new Category {categoryName = "Детские", description="Книги для детей",},
+                    new Category {categoryName = "Детективы", description="Таинтвенные истории",},
+                    new Category {categoryName = "Научная фантастика", description="Путешествия в мирах высоких технологий"},
+                    new Category {categoryName = "Приключения", description="Истории об отважных путешественниках"}
+
+                };
+                
+
+
+            }
+        
+        
+        }
+    }
+}
