816f09cc937ac711da59120353a220a51e321a61
Jager227 <82110630+Jager227@users.noreply.github.com>
2021-04-14 20:12:03 +0300

    Добавил Класс Издательство(Не доделал)

---
+++ Syte/Models/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Syte.Models
+{
+    public class Publisher
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Adress { set; get; }
+    }
+}
