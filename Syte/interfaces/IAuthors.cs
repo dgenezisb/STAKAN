4b44bec2228494cf7cb706067e947f5c834676b5
Maksim <82108446+maks2179@users.noreply.github.com>
2021-04-17 02:04:23 +0300

    Комменты

--- Syte/interfaces/IAuthors.cs
+++ Syte/interfaces/IAuthors.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Syte.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Syte.Models;
 
-namespace Syte.Interfaces
-{
-    interface IAuthors      //Интерфейс для работы с авторами
-    {
-        IEnumerable<Authors> ListofAuthors { get;} //Функция получает список всех авторов
-    }
-}
+//namespace Syte.Interfaces
+//{
+//    public interface IAuthors      //Интерфейс для работы с авторами
+//    {
+//        IEnumerable<Authors> ListofAuthors { get;} //Функция получает список всех авторов
+//    }
+//}
