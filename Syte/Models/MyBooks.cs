11293abcaa6737930c908e6e64ab851c39648de5
Maksim <82108446+maks2179@users.noreply.github.com>
2021-05-10 00:42:38 +0300

    1

--- Syte/Models/MyBooks.cs
+++ Syte/Models/MyBooks.cs
@@ -14,7 +14,6 @@ namespace Syte.Models
         public int Id { set; get; }
         public string Name { get; set; }
         public List<Book> Books { get; set; } = new List<Book>();
-        //public List<Book> Books { set; get; }
         public int UserID { set; get; }
         [ForeignKey(nameof(UserID))]
         public virtual User User { set; get; }
--- Syte/Models/Tags.cs
+++ Syte/Models/Tags.cs
@@ -9,26 +9,10 @@ namespace Syte.Models
 {
     public class Tags
     {
-        private Guid guid;
-        private string v;
-        private Book book;
-
-        public Tags(Guid guid, string v, Book book, Book book1)
-        {
-            this.guid = guid;
-            this.v = v;
-            this.book = book;
-        }
-
-        public Tags()
-        {
-        }
-
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { set; get; }
         public string Name { set; get; }
-        //public List<Book> Books { set; get; }
         public List<Book> Books { get; set; } = new List<Book>();
     }
 }
