9c6edb21b6d753d8bff810d2d8995197bc264dff
Jager227 <82110630+Jager227@users.noreply.github.com>
2021-05-10 00:41:45 +0300

    Добавление книг на сайте(не рабочее)

--- Syte/Controllers/BooksController.cs
+++ Syte/Controllers/BooksController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Syte.Interfaces;
+using Syte.Models;
 using Syte.ViewModels;
 
 namespace Syte.Controllers
@@ -12,6 +14,7 @@ namespace Syte.Controllers
     {
         private readonly IAllBooks _allBooks;
         private readonly ICategories _allCategories;
+        private AppDBContext db;
 
         public BooksController(IAllBooks IAllBooks, ICategories IBooksCategories)
         {
@@ -26,5 +29,35 @@ namespace Syte.Controllers
             obj.CurrentCategory = "Тут должно быть название жанра";
             return View(obj);
         }
-    }
+        public ActionResult Create()
+        {
+            Book book = new Book();
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Book.Add(book);
+                    db.SaveChanges();
+                    return RedirectToAction("List");
+                }
+            
+            }
+            catch(Exception ex)
+            {
+                //ModelState.AddModelError(String.Empty, ex);
+            }
+            return View(book);
+        }
+
+
+        }
+
+
+
 }
+
---
+++ Syte/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syte.Interfaces;
+
+namespace Syte.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUser _allUsers;
+    }
+  
+
+
+
+}
